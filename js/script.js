2e0a092cf7977a31037191c1f4f9643583562ec8
Rubén <gonzru01atwork@gmail.com>
2022-09-12 14:28:34 -0400

    work input form basic frontend done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let radioEmbFalse = document.querySelector('#false');
+
+const changeVisibility = () => {
+    if (radioEmbFalse.checked === true) {
+        document.querySelector('.embroidery-true').style.display = 'none';
+    } else {
+        document.querySelector('.embroidery-true').style.display = 'initial';
+    }
+    
+}
