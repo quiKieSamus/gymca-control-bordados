87c10df2f7fa63456f318bccbe5b7594a2a4a51e
Rubén <gonzru01atwork@gmail.com>
2022-09-18 22:58:55 -0400

    workInput form making

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-let radioEmbFalse = document.querySelector('#false');
-
-const changeVisibility = () => {
-    if (radioEmbFalse.checked === true) {
-        document.querySelector('.embroidery-true').style.display = 'none';
-    } else {
-        document.querySelector('.embroidery-true').style.display = 'initial';
+const changeVisibility = (n, bool) => {
+    let showDivElemArr = document.querySelectorAll('.showDiv')
+    for(let i = 0; i < showDivElemArr.length; i++) {
+        let showDivElem = document.querySelector(`.showDiv${i}`);
+        if (i === n) {
+            if (bool == false){
+                showDivElem.style.display = 'none';
+                console.log("me desaparezco en: ", showDivElem);
+            } else {
+                showDivElem.style.display = 'initial';
+            }
+            
+        }
     }
     
 }
 
-addEventListener('DOMContentLoaded', changeVisibility());
+const onSubmitEvent = (n /*embroidery number*/) => {
+
+}
+
+addEventListener('DOMContentLoaded', () => {
+    for(let i = 0; i < document.querySelectorAll('.showDiv').length; i++) {
+        document.querySelectorAll('.showDiv')[i].style.display = 'none';
+    }
+});
