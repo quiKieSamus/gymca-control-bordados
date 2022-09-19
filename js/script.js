74fdfce8b9753478ff1e37be71e16d541808732a
Rubén <gonzru01atwork@gmail.com>
2022-09-19 13:58:28 -0400

    workInput form done (I hope)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 const changeVisibility = (n, bool) => {
     let showDivElemArr = document.querySelectorAll('.showDiv')
-    for(let i = 0; i < showDivElemArr.length; i++) {
+    for (let i = 0; i < showDivElemArr.length; i++) {
         let showDivElem = document.querySelector(`.showDiv${i}`);
         if (i === n) {
-            if (bool == false){
+            if (bool == false) {
+                if (n === 0) {
+                    document.querySelector("#embClothQty").required = false;
+                    document.querySelector("#embXCloth").required = false;
+                }
                 showDivElem.style.display = 'none';
-                console.log("me desaparezco en: ", showDivElem);
             } else {
+                if (n === 0) {
+                    document.querySelector("#embClothQty").required = true;
+                    document.querySelector("#embXCloth").required = true;
+
+                }
                 showDivElem.style.display = 'initial';
+
             }
-            
+
         }
     }
-    
+
 }
 
 addEventListener('DOMContentLoaded', () => {
-    for(let i = 0; i < document.querySelectorAll('.showDiv').length; i++) {
+    for (let i = 0; i < document.querySelectorAll('.showDiv').length; i++) {
         document.querySelectorAll('.showDiv')[i].style.display = 'none';
     }
 });
