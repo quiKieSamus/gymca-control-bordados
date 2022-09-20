1cd3ab14d963517db42edf6d427f0732d0ee9d64
Rubén <gonzru01atwork@gmail.com>
2022-09-20 00:31:05 -0400

    work input is complete, now I need to add confection number of clothes field in workInput.html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 const changeVisibility = (n, bool) => {
     let showDivElemArr = document.querySelectorAll('.showDiv')
+    const embClothQty =document.querySelector("#embClothQty");
+    const embXCloth = document.querySelector("#embXCloth");
+    const backEmbQty = document.querySelector("#backEmbQty");
     for (let i = 0; i < showDivElemArr.length; i++) {
         let showDivElem = document.querySelector(`.showDiv${i}`);
         if (i === n) {
             if (bool == false) {
                 if (n === 0) {
-                    document.querySelector("#embClothQty").required = false;
-                    document.querySelector("#embXCloth").required = false;
+                    embClothQty.required = false;
+                    embXCloth.required = false;
+                } else if (n === 1) {
+                    backEmbQty.required = false;
                 }
                 showDivElem.style.display = 'none';
             } else {
                 if (n === 0) {
-                    document.querySelector("#embClothQty").required = true;
-                    document.querySelector("#embXCloth").required = true;
+                    embClothQty.required = true;
+                    embXCloth.required = true;
 
+                } else if (n === 1) {
+                    backEmbQty.required = true;
                 }
                 showDivElem.style.display = 'initial';
 
--- php/workInput.php
+++ php/workInput.php
@@ -1,4 +1,48 @@
-<?php 
+<?php
+include("./dbConfig/config.php");
 
+if (isset($_POST['doc']) && isset($_POST['description'])) {
+    $doc = $_POST['doc'];
+    $desc = $_POST['description'];
+    $emb = $_POST['embroidery'];
+    $embBack = $_POST['back'];
+    $sql0 = "SELECT documento FROM clientes WHERE documento='$doc'";
+    $sql0Query = mysqli_query($con, $sql0);
+    $sql0Result = mysqli_fetch_array($sql0Query);
+    if (mysqli_num_rows(mysqli_query($con, $sql0)) > 0) {
+        //client exists and the order is processed
+        if ($emb === 'true') {
+            //work has embroidery
+            if (isset($_POST['embClothQty']) && isset($_POST['embXCloth'])) {
+                $embClothQty = $_POST['embClothQty'];
+                $embXCloth = $_POST['embXCloth'];
+                $total = ($embClothQty * $embXCloth * 0.2);
 
-?>
+                if ($embBack === 'true') {
+                    //clothes have back embroidery
+                    if (isset($_POST['backEmbQty'])) {
+                        $backEmbQty = $_POST['backEmbQty'];
+                        $total += ($backEmbQty * 0.3);
+                    } else {
+                        //data was set incorrectly 
+                        echo "<h1>Los datos fueron ingresados incorrectamente</h1>";
+                    }
+                }
+                $sql1 = "INSERT INTO trabajo(descripcion, cantidad, total, cliente, bordadoPrenda) VALUES('$desc', '$embClothQty', '$total', '$sql0Result[documento]', '$embXCloth')";
+                mysqli_query($con, $sql1);
+            } else {
+                //information was set incorrectly
+                echo "<h1>Los datos fueron ingresados incorrectamente</h1>";
+            }
+        } else {
+            $sqlToInsert0 = "INSERT INTO trabajo(descripcion, cliente) VALUES ('$desc', '$sql0Result[documento]')";
+            mysqli_query($con, $sqlToInsert0);
+        }
+    } else {
+        //client doesn't exists and the creation of the new client must be done 
+        echo "<h1>Cliente no existe, cree uno nuevo <a href='../clientInput.html'>acá</a></h1>";
+    }
+} else {
+    //data was set incorrectly
+    echo "<h1>Los datos fueron ingresados incorrectamente</h1>";
+}
