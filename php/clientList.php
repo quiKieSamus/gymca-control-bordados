ce819a8eaa9bef555365237dab8c38d1dd8b1684
Rubén <gonzru01atwork@gmail.com>
2022-09-20 12:33:43 -0400

    info shown in tables

--- php/clientList.php
+++ php/clientList.php
@@ -10,16 +10,29 @@
 
 <body>
     <div>
+        <label for="searchBar">Buscar</label>
         <input type="text" name="searchBar" id="searchBar" class="searchTxt">
     </div>
     <?php
     include("./dbConfig/config.php");
     $sql = "SELECT * FROM CLIENTES";
     $sqlQuery = mysqli_query($con, $sql);
+    echo "<table>     
+    <tr>
+        <th>Nombre</th>
+        <th>Documento</th>    
+        <th>Trabajos Realizados</th>    
+        </tr>";
     while ($row = mysqli_fetch_array($sqlQuery)) {
-        echo $row['nombre'] . "<br>";
-    }
 
+        
+        echo"<tr>
+            <td>$row[documento]</td>
+            <td>$row[nombre]</td>
+            <td>$row[trabajo]</td>
+        </tr>";
+    }
+    echo "</table>";
     ?>
 </body>
 
